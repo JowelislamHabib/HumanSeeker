16ce33f7b7381467a74a2eac6567170484636899
JowelislamHabib <jowel@bintofajjal.com>
2026-06-03 17:35:41 +0600

    feat: add agency jobs listing and create new job pages

---
+++ src/app/dashboard/agency/jobs/new/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateNewJobPage = () => {
+  return <div>This is an Add New Job Page</div>;
+};
+
+export default CreateNewJobPage;
---
+++ src/app/dashboard/agency/jobs/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AgenciesJobsPage = () => {
+  return <div>This is a Agencies job page</div>;
+};
+
+export default AgenciesJobsPage;
