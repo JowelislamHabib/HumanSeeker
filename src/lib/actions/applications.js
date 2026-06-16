2ba03bfff6d0440d4065aec8d705c804284953ad
JowelislamHabib <jowel@bintofajjal.com>
2026-06-16 23:36:05 +0600

    feat: prepend basePath to application submission API endpoint

--- src/lib/actions/applications.js
+++ src/lib/actions/applications.js
@@ -1,7 +1,8 @@
 "use server";
 
+import { basePath } from "@/utils/EnvConfig";
 import { serverMutation } from "../core/server";
 
 export const submitApplication = async (applicationData) => {
-  return await serverMutation("/api/applications", applicationData);
+  return await serverMutation(`${basePath}/api/applications`, applicationData);
 };
